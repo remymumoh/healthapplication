8187fcd364ebb590e958176382b4a77151928f4b
Remy Mumo <remymumoh@gmail.com>
2025-08-18 16:08:30 +0300

    Fix react-native-screens peer dependency conflict

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -5,10 +5,10 @@ export * from 'expo-router';
 
 declare module 'expo-router' {
   export namespace ExpoRouter {
-    export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/care-treatment` | `/(tabs)/hts` | `/_sitemap` | `/care-treatment` | `/hts`;
-      DynamicRoutes: never;
-      DynamicRouteTemplate: never;
+    export interface __routes<T extends string | object = string> {
+      hrefInputParams: { pathname: Router.RelativePathString, params?: Router.UnknownInputParams } | { pathname: Router.ExternalPathString, params?: Router.UnknownInputParams } | { pathname: `/_sitemap`; params?: Router.UnknownInputParams; } | { pathname: `${'/(tabs)'}/care-treatment` | `/care-treatment`; params?: Router.UnknownInputParams; } | { pathname: `${'/(tabs)'}/hts` | `/hts`; params?: Router.UnknownInputParams; } | { pathname: `${'/(tabs)'}` | `/`; params?: Router.UnknownInputParams; } | { pathname: `/+not-found`, params: Router.UnknownInputParams & {  } };
+      hrefOutputParams: { pathname: Router.RelativePathString, params?: Router.UnknownOutputParams } | { pathname: Router.ExternalPathString, params?: Router.UnknownOutputParams } | { pathname: `/_sitemap`; params?: Router.UnknownOutputParams; } | { pathname: `${'/(tabs)'}/care-treatment` | `/care-treatment`; params?: Router.UnknownOutputParams; } | { pathname: `${'/(tabs)'}/hts` | `/hts`; params?: Router.UnknownOutputParams; } | { pathname: `${'/(tabs)'}` | `/`; params?: Router.UnknownOutputParams; } | { pathname: `/+not-found`, params: Router.UnknownOutputParams & {  } };
+      href: Router.RelativePathString | Router.ExternalPathString | `/_sitemap${`?${string}` | `#${string}` | ''}` | `${'/(tabs)'}/care-treatment${`?${string}` | `#${string}` | ''}` | `/care-treatment${`?${string}` | `#${string}` | ''}` | `${'/(tabs)'}/hts${`?${string}` | `#${string}` | ''}` | `/hts${`?${string}` | `#${string}` | ''}` | `${'/(tabs)'}${`?${string}` | `#${string}` | ''}` | `/${`?${string}` | `#${string}` | ''}` | { pathname: Router.RelativePathString, params?: Router.UnknownInputParams } | { pathname: Router.ExternalPathString, params?: Router.UnknownInputParams } | { pathname: `/_sitemap`; params?: Router.UnknownInputParams; } | { pathname: `${'/(tabs)'}/care-treatment` | `/care-treatment`; params?: Router.UnknownInputParams; } | { pathname: `${'/(tabs)'}/hts` | `/hts`; params?: Router.UnknownInputParams; } | { pathname: `${'/(tabs)'}` | `/`; params?: Router.UnknownInputParams; } | `/+not-found` | { pathname: `/+not-found`, params: Router.UnknownInputParams & {  } };
     }
   }
 }
